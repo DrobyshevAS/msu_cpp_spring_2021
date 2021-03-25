0d90443d28de260f603ab89b592e56fe00ec33fb
DrobyshevAS <38296481+DrobyshevAS@users.noreply.github.com>
2021-03-25 21:12:45 +0300

    Add files via upload

---
+++ 01/allocator.cpp
@@ -0,0 +1,33 @@
+#include "allocator.hpp"
+
+
+void Allocator::makeAllocator(size_t maxSize)
+{
+    if (buf != nullptr)
+    {
+        delete[] buf;
+    }
+    buf = new char[maxSize];
+    capacity = maxSize;
+    offset = 0;
+}
+
+char* Allocator::alloc(size_t size)
+{
+    if ((size <= 0) || (size + offset > capacity))
+    {
+        return nullptr;
+    }
+    offset += size;
+    return buf + offset - size;
+}
+
+void Allocator::reset()
+{
+    offset = 0;
+}
+
+Allocator::~Allocator()
+{
+    delete[] buf;
+}
---
+++ 01/allocator.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdio.h>
+
+class Allocator
+{
+private:
+    char *buf = nullptr;
+    size_t offset;
+    size_t capacity;
+
+
+public:
+    void makeAllocator(size_t maxSize);
+    char* alloc(size_t size);
+    void reset();
+    ~Allocator();
+};

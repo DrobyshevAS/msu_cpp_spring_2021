76dbb911333abc74b19fba72184e7f5c497781ca
DrobyshevAS <38296481+DrobyshevAS@users.noreply.github.com>
2021-05-31 19:45:57 +0300

    Add files via upload

--- 06/exceptions.hpp
+++ 06/exceptions.hpp
@@ -6,43 +6,43 @@
 
 class BaseError : public std::logic_error 
 {
-   public:
-    BaseError(const std::string& error_log) : std::logic_error(error_log) 
-    {
-        std::ostringstream error_stream;
-        error_stream  << error_log;
-        msg_ = error_stream.str();
-    }
+    public:
+        BaseError(const std::string& error_log) : std::logic_error(error_log) 
+        {
+            std::ostringstream error_stream;
+            error_stream  << error_log;
+            err_ = error_stream.str();
+        }
 
-    const char* what() const throw() override 
-    {
-        return msg_.c_str();
-    }
+        const char* what() const throw() override 
+        {
+            return err_.c_str();
+        }
 
-   private:
-    std::string msg_;
+    private:
+        std::string err_;
 };
 
 class TooManyArgumentsError : public BaseError 
 {
-   public:
-    TooManyArgumentsError(const std::string& error_log) : BaseError(error_log) {}
+    public:
+        TooManyArgumentsError(const std::string& error_log) : BaseError(error_log) {}
 };
 
 class NotEnoughArgumentsError : public BaseError 
 {
-   public:
-    NotEnoughArgumentsError(const std::string& error_log) : BaseError(error_log) {}
+    public:
+        NotEnoughArgumentsError(const std::string& error_log) : BaseError(error_log) {}
 };
 
 class IncorrectDataBetweenBrackets : public BaseError 
 {
-   public:
-    IncorrectDataBetweenBrackets(const std::string& error_log) : BaseError(error_log) {}
+    public:
+        IncorrectDataBetweenBrackets(const std::string& error_log) : BaseError(error_log) {}
 };
 
 class BracketError : public BaseError 
 {
-   public:
-    BracketError(const std::string& error_log) : BaseError(error_log) {}
+    public:
+        BracketError(const std::string& error_log) : BaseError(error_log) {}
 };
